d504bd007a9da42c6322d21706d661526a926234
Harman Barpaga <harmanbarpaga1@gmail.com>
2024-04-18 23:34:38 -0400

    new commit

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -6,14 +6,14 @@ function Forecast({title, items}) {
     <div>
 
         <div className="flex items-center justify-start mt-6">
-             <p className="text-white font-medium uppercase"> {title}</p>
+             <p className="text-white font-medium uppercase"> {`${title}`}</p>
         </div>
         <hr className="my-2"/>
 
         <div className="flex flex-row items-center justify-between text-white">
             {items.map((item) => (
                 <div className="flex flex-col items-center justify-center">
-                <p className="font-light text-sm">{item.title}</p>
+                <p className="font-light text-sm">{`${item.title}`}</p>
                 <img src={iconUrlFromCode(item.icon)}
                 className="w-12 my-1" alt="" />
                 <p className="font-medium">{`${item.temp.toFixed()}°`}</p>
