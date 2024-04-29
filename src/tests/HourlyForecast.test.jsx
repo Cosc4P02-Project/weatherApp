7268334e200d000a7ba74f5b6da6afe76cf7e23e
Gonicku <gonicku@gmail.com>
2024-04-28 21:41:34 -0400

    Test fixes

--- src/tests/HourlyForecast.test.jsx
+++ src/tests/HourlyForecast.test.jsx
@@ -1,4 +1,3 @@
-//import { fireEvent, render, screen } from "@testing-library/react";
 import { fireEvent, render, screen } from '@testing-library/react';
 import HourlyForecast from "../components/HourlyForecast";
 import getFormattedWeatherData from "../components/services/weatherService";
@@ -11,18 +10,17 @@ describe(HourlyForecast, () => {
     })
 
     it("Initializes to the first page of weather data.", () => {
-        //render(<HourlyForecast title="Hourly" items={weather} />)
         render(<HourlyForecast title = "Hourly" items = {weather}/>);
-        const pageElement = screen.getByTestId("page") // Not grabbing
+        const pageElement = screen.getByTestId("page")
         //expect(pageElement).not.toEqual(null)
-        //const pageNumber = screen.getByTestId("page").value
         const pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
         expect(pageNumber).toEqual(1)
     })
+
     it("Increments and does not pass the seventh page", () => {
         render(<HourlyForecast title = "Hourly" items = {weather} />)
-        var pageNumber = Number(screen.getByTestId("page").getAttribute('value'))  // Not grabbing
-        const incrementPage = screen.getByRole("button", { name: '››' }) // Not grabbing - easy fix
+        var pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
+        const incrementPage = screen.getByRole("button", { name: '››' })
 
         for (var i = 1; i < 8; i++) {
             expect(pageNumber).toEqual(i)
@@ -31,6 +29,7 @@ describe(HourlyForecast, () => {
         }
         expect(pageNumber).toEqual(7)
     })
+
     it("Decrements and does not pass the seventh page", () => {
         render(<HourlyForecast title="Hourly" items={weather} />)
         var pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
@@ -48,6 +47,7 @@ describe(HourlyForecast, () => {
         }
         expect(pageNumber).toEqual(1)
     })
+    
     it("Resets page number on weather data change", () => {
         render(<HourlyForecast title = "Hourly" items = {weather} />)
         var pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
@@ -64,7 +64,6 @@ describe(HourlyForecast, () => {
             screen.HourlyForecast.items = londonWeather
             pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
             expect(pageNumber).toEqual(1)
-            // Could have a timing problem - got upset with having expect in this .then() promise catch
         })
     })
 })
